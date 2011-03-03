ebe75261de99da7ea3145407a339a11434dc5633
lps_lp_sk8 <lps_lp_sk8@hotmail.com>
2011-03-03 13:27:02 +0000

    - Adição de imagens ilustrativas.

--- frmPrincipal.Designer.cs
+++ frmPrincipal.Designer.cs
@@ -29,7 +29,7 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.btnPlay = new System.Windows.Forms.Button();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmPrincipal));
             this.pnlFrases = new System.Windows.Forms.Panel();
             this.lblNomeMusica = new System.Windows.Forms.Label();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
@@ -38,25 +38,16 @@
             this.cadastrarMusicaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.btnStop = new System.Windows.Forms.Button();
+            this.btnPlay = new System.Windows.Forms.Button();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // btnPlay
-            // 
-            this.btnPlay.Location = new System.Drawing.Point(121, 161);
-            this.btnPlay.Name = "btnPlay";
-            this.btnPlay.Size = new System.Drawing.Size(244, 23);
-            this.btnPlay.TabIndex = 0;
-            this.btnPlay.Text = "Play";
-            this.btnPlay.UseVisualStyleBackColor = true;
-            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
             // 
             // pnlFrases
             // 
             this.pnlFrases.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.pnlFrases.Location = new System.Drawing.Point(12, 49);
             this.pnlFrases.Name = "pnlFrases";
-            this.pnlFrases.Size = new System.Drawing.Size(703, 89);
+            this.pnlFrases.Size = new System.Drawing.Size(703, 56);
             this.pnlFrases.TabIndex = 1;
             // 
             // lblNomeMusica
@@ -87,8 +78,11 @@
             // 
             this.ferramentasToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.cadastrarMusicaToolStripMenuItem});
+            this.ferramentasToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("ferramentasToolStripMenuItem.Image")));
+            this.ferramentasToolStripMenuItem.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.ferramentasToolStripMenuItem.ImageTransparentColor = System.Drawing.Color.Transparent;
             this.ferramentasToolStripMenuItem.Name = "ferramentasToolStripMenuItem";
-            this.ferramentasToolStripMenuItem.Size = new System.Drawing.Size(84, 20);
+            this.ferramentasToolStripMenuItem.Size = new System.Drawing.Size(100, 20);
             this.ferramentasToolStripMenuItem.Text = "Ferramentas";
             // 
             // cadastrarMusicaToolStripMenuItem
@@ -104,27 +98,42 @@
             // 
             // btnStop
             // 
+            this.btnStop.BackColor = System.Drawing.SystemColors.ControlLight;
             this.btnStop.Enabled = false;
-            this.btnStop.Location = new System.Drawing.Point(371, 161);
+            this.btnStop.Image = global::Karaoke.Properties.Resources.stop;
+            this.btnStop.Location = new System.Drawing.Point(360, 111);
             this.btnStop.Name = "btnStop";
-            this.btnStop.Size = new System.Drawing.Size(244, 23);
-            this.btnStop.TabIndex = 8;
-            this.btnStop.Text = "Stop";
-            this.btnStop.UseVisualStyleBackColor = true;
-            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            this.btnStop.Size = new System.Drawing.Size(80, 80);
+            this.btnStop.TabIndex = 12;
+            this.btnStop.UseVisualStyleBackColor = false;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click_1);
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.btnPlay.Image = global::Karaoke.Properties.Resources.play;
+            this.btnPlay.Location = new System.Drawing.Point(274, 111);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(80, 80);
+            this.btnPlay.TabIndex = 11;
+            this.btnPlay.UseVisualStyleBackColor = false;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click_1);
             // 
             // frmPrincipal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(727, 222);
+            this.ClientSize = new System.Drawing.Size(727, 203);
             this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnPlay);
             this.Controls.Add(this.lblNomeMusica);
             this.Controls.Add(this.pnlFrases);
-            this.Controls.Add(this.btnPlay);
             this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "frmPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Karaoke";
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
@@ -135,7 +144,6 @@
 
         #endregion
 
-        private System.Windows.Forms.Button btnPlay;
         private System.Windows.Forms.Panel pnlFrases;
         private System.Windows.Forms.Label lblNomeMusica;
         private System.Windows.Forms.Timer timer1;
@@ -143,6 +151,7 @@
         private System.Windows.Forms.ToolStripMenuItem ferramentasToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem cadastrarMusicaToolStripMenuItem;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnPlay;
         private System.Windows.Forms.Button btnStop;
     }
 }
