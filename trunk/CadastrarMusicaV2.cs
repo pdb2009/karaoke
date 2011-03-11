53b931b61937f07fd5b0954ccd13428cadce4baa
lps_lp_sk8@hotmail.com <lps_lp_sk8@hotmail.com@87c446cb-9c37-7691-40d5-5039db00c985>
2011-03-11 12:56:14 +0000

    - Criação de nova ferramenta para cadastrar músicas (V2)

---
+++ trunk/CadastrarMusicaV2.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using Microsoft.VisualBasic.FileIO;
+
+namespace Karaoke
+{
+    public partial class CadastrarMusicaV2 : Form
+    {
+        public String caminhoMusica;
+        public List<String> textoFrases;
+        private List<Frase> frases;
+
+        internal List<Frase> Frases
+        {
+            get { return frases; }
+            set { frases = value; }
+        }
+
+        public CadastrarMusicaV2()
+        {
+            InitializeComponent();
+            textoFrases = new List<string>();
+        }
+
+        private void btnProcurar_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                caminhoMusica = openFileDialog1.FileName;
+                txtCaminho.Text = caminhoMusica;
+            }
+        }
+
+        private void btnAddFrase_Click(object sender, EventArgs e)
+        {            
+            txtFrase.Text = txtFrase.Text.Replace('\n', ' ');
+            txtFrase.Text = txtFrase.Text.Replace('\r', ' ');
+            String[] letra = txtFrase.Text.Split(' ');
+            foreach (String frase in letra)
+            {
+                frase.Trim();
+                if (!String.IsNullOrEmpty(frase))
+                {
+                    textoFrases.Add(frase);
+                    lstFrases.Items.Add(frase);
+                }
+            }            
+            txtFrase.Text = "";
+            txtFrase.Focus();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            new CadastrarTimming(this).ShowDialog(this);
+        }
+
+        private void btnRemover_Click(object sender, EventArgs e)
+        {
+            foreach (ListViewItem item in lstFrases.SelectedItems)
+            {
+                lstFrases.Items.Remove(item);
+            }
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(txtCaminho.Text) || String.IsNullOrEmpty(txtNome.Text))
+            {
+                MessageBox.Show("Informe o nome e o arquivo da música.", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            String caminhoOrigem = txtCaminho.Text;
+            String nomeArquivo = caminhoOrigem.Substring(caminhoOrigem.LastIndexOf("\\") + 1).Replace(' ', '_');
+            String caminhoDestino = Path.Combine(Application.StartupPath + "\\media", nomeArquivo);
+            try
+            {
+                if (FileSystem.FileExists(Application.StartupPath + "\\musicas\\" + txtNome.Text + ".kar"))
+                {
+                    MessageBox.Show("Nome de música já existe. Escolha outro.", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                if (FileSystem.FileExists(caminhoDestino))
+                {
+                    MessageBox.Show("Arquivo de música já existente. Altere o nome do arquivo a ser importado.", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                FileSystem.CopyFile(caminhoOrigem, caminhoDestino, UIOption.AllDialogs);
+                StreamWriter wr = new StreamWriter(Application.StartupPath + "\\musicas\\" + txtNome.Text + ".kar", false);
+                wr.WriteLine(txtNome.Text);
+                wr.WriteLine(nomeArquivo);
+                foreach (Frase frase in frases)
+                {
+                    wr.WriteLine(frase.Texto + "#" + frase.TempoInicio.ToString() + "#" + frase.TempoFim.ToString());
+                }
+                wr.Close();
+                MessageBox.Show("Música cadastrada com sucesso.", "Concluído", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.Dispose();
+            }
+            catch
+            {
+                MessageBox.Show("Não foi possível cadastrar a música.", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
---
+++ trunk/CadastrarTimming.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using IrrKlang;
+
+namespace Karaoke
+{
+    public partial class CadastrarTimming : Form
+    {
+        private List<String> textoFrases;
+        private List<Frase> frases;
+        private ISound musica;
+        private int contador = 0;
+        private uint lastTempoInicio = 0;
+        private Frase lastFrase = null;
+        CadastrarMusicaV2 frmPai;
+
+        public CadastrarTimming(CadastrarMusicaV2 frmPai)
+        {
+            InitializeComponent();
+            
+            this.frmPai = frmPai;
+            this.textoFrases = frmPai.textoFrases;
+            if (textoFrases == null || textoFrases.Count < 1)
+            {
+                this.Dispose();
+                return;
+            }
+            this.frases = new List<Frase>();
+            btnFrase.Enabled = false;
+            btnSpace.Enabled = false;
+            MessageBox.Show("Nesta tela você poderá entrar com a música com vocal para ajudar na gravação do tempo das falas, porém esta deve ter os mesmos tempos que a música de playback para que a letra esteja em sincronia. Caso não queira, forneça a música de playback novamente.", "Cadastrar Tempo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }   
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            if (btnPlay.Text.Equals("Stop"))
+            {
+                musica.Stop();
+                btnPlay.Text = "Play";
+                btnFrase.Enabled = false;
+                btnSpace.Enabled = false;
+            }
+            else
+            {
+                openFileDialog1.InitialDirectory = Application.StartupPath + "\\musicas\\";
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    btnFrase.Enabled = true;
+                    btnSpace.Enabled = true;
+                    btnPlay.Text = "Stop";
+                    ISoundEngine engine = new ISoundEngine();
+                    musica = engine.Play2D(openFileDialog1.FileName);
+                    this.Focus();
+                }
+            }
+        }
+
+        private void btnFrase_Click(object sender, EventArgs e)
+        {
+            if (musica.Finished || contador > textoFrases.Count - 1)
+            {
+                finalizar();
+            }
+
+            if (lastFrase == null)
+            {
+                Frase frase = new Frase(textoFrases[contador], musica.PlayPosition - 50, musica.PlayLength);
+                frases.Add(frase);
+                lstFrases.Items.Add(frase);
+                lstFrases.SelectedItem = frase;
+                lastFrase = frase;
+                contador++;
+            }
+            else
+            {
+                frases.Remove(lastFrase);
+                lstFrases.Items.Remove(lastFrase);
+                lastFrase.TempoFim = musica.PlayPosition - 50;
+                frases.Add(lastFrase);
+                lstFrases.Items.Add(lastFrase);
+
+                Frase frase = new Frase(textoFrases[contador], musica.PlayPosition - 50, musica.PlayLength);
+                frases.Add(frase);
+                lstFrases.Items.Add(frase);
+                lstFrases.SelectedItem = frase;
+                lastFrase = frase;
+                contador++;
+            }          
+        }
+
+        private void btnSpace_Click(object sender, EventArgs e)
+        {
+            if (musica.Finished || contador > textoFrases.Count - 1)
+            {
+                finalizar();
+            }
+
+            if (lastFrase != null)
+            {
+                frases.Remove(lastFrase);
+                lstFrases.Items.Remove(lastFrase);
+                lastFrase.TempoFim = musica.PlayPosition;
+                frases.Add(lastFrase);
+                lstFrases.Items.Add(lastFrase);
+                lstFrases.SelectedItem = lastFrase;
+                lastFrase = null;                
+            }            
+        }
+
+        private void finalizar()
+        {
+            musica.Stop();
+            frmPai.Frases = this.frases;
+            this.Dispose();
+        }
+    }
+}
